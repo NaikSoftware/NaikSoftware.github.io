a594afb48d6b831b22cb5bbfb88dac7a4b73d2e6
User <yana95.95@inbox.ru>
2015-05-18 01:16:14 +0300

    Add ddnf calc, fix code

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var varNum = 2;
 var MAX_VAR_NUM = 6;
 var MIN_VAR_NUM = 2;
 
-window.onload = function () {
+$(function () {
     genTable();
 
     $("#btn_plus").click(function () {
@@ -20,64 +20,87 @@ window.onload = function () {
     });
     
     $("#btn_ddnf").click(function () {
-        calcDdnf($("#latexMode").checked);
+        calcDdnf($("#latexMode").get(0).checked);
     });
     
     $("#btn_ddnf").click(function () {
-        calcDknf($("#latexMode").checked);
+        calcDknf($("#latexMode").get(0).checked);
     });
-};
+});
 
 function genTable() {
     // Gen head
     $("#label_num").text(varNum);
-    var head = $("#truth_table_head");
-    head.empty();
+    var head = $("#truth_table_head").empty();
     for (i = 1; i <= varNum; i++) {
         head.append("<td><div class=\"text-center text-primary\">X<sub>" + i + "</sub></div></td>");
     }
     head.append("<td class=\"col-lg-2\"><div class=\"text-center text-warning\">F<sub>" + 1 + "</sub></div></td>");
 
     // Gen body
-    var body = $("#truth_table_body");
-    body.empty();
+    var body = $("#truth_table_body").empty();
     var rows = Math.pow(2, varNum);
     for (i = 0; i < rows; i++) {
-        var row = $("<tr></tr>");
+        var row = $("<tr>");
         for (j = 1; j <= varNum; j++) {
             var n = Math.pow(2, varNum - j);
             var m = Math.floor(i / n);
             row.append("<td>" + (m % 2 === 0 ? "0" : "1") + "</td>");
         }
-        row.append("<td><div class=\"col-lg-9\"><div class=\"input-group\">"
-                + "<span class=\"input-group-addon\"><input type=\"checkbox\" onclick=\"clickCheckbox(this, " + i + ")\"></span>"
-                + "<input type=\"text\" id=\"f" + i + "\" class=\"form-control\" disabled value=\"0\"></div></td>");
+        row.append('<td class="col-lg-9"><div class="input-group">'
+                + '<span class="input-group-addon"><input type="checkbox" onclick="clickCheckbox(this, ' + i + ')"></span>'
+                + '<input type="text" id="f' + i + '" class="form-control" disabled value="0"></div></td>');
         body.append(row);
     }
 }
 
 function clickCheckbox(chBox, id) {
-    var input = $("#f" + id);
+    var input = $('#f' + id);
     if (chBox.checked)
-        input.val("1");
+        input.val('1');
     else
-        input.val("0");
+        input.val('0');
 }
 
 function getTableContent() {
     var result = [];
+	var func = [];
     var body = $("#truth_table_body");
-    body.children().each(function (idx, val) {
-        result[idx] = val;
+    body.children().each(function (idx, valTr) {
+		var row = [];
+        $(valTr).children().each(function (idx2, valTd) {
+			if (valTd.innerText !== '') {
+				row[idx2] = valTd.innerText;
+			} else {
+				func[idx] = $(valTd).find("input[type=text]").val();
+			}
+		});
+		result[idx] = row;
     });
-    return result;
+    return {tbl: result, fnc: func};
 }
 
 function calcDdnf(latexMode) {
     var data = getTableContent();
     var text = $("#result_text").empty();
-    text.append(data.toString());
-    console.log("Data: " + data.toString());
+	text.append("ДДНФ(F<sub>1</sub>) = ");
+	var rows = data.fnc.length;
+	var implicants = 0;
+	for (var i = 0;i < rows;i++) {
+		if (data.fnc[i] === "1") {
+			var impl = (implicants? ' + ' : '') + '<i>';
+			implicants++;
+			for (var j = 0;j < varNum;j++) {
+				if (data.tbl[i][j] === "1") {
+					impl += ('x<sub>' + (j + 1) + '</sub>');
+				} else {
+					impl += ('<span class="overline">x<sub>' + (j + 1) + '</sub></span>');
+				}
+			}
+			impl += "</i>";
+			text.append(impl);
+		}
+	}
 }
 
 function calcDknf(latexMode) {
